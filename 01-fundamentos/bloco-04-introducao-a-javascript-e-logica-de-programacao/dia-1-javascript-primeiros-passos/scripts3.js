a5d4ca434a33bc7139a2bab32022004dc989bb03
Renanbfreitas <renanbf1992@outlook.com>
2022-07-21 12:22:34 -0300

    Organiza diretorios do dia

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts3.js
@@ -0,0 +1,11 @@
+const a = 20;
+const b = 10;
+const c = 5;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+};
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts9.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 15;
+const c = 25;
+
+let isOdd = false;
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+  isOdd = true;
+};
+console.log(isOdd);
