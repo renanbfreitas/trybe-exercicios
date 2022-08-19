d0d3a19a449f0300f434c3bc39518015f8900c2a
Renanbfreitas <renanbf1992@outlook.com>
2022-08-19 20:22:13 -0300

    Organiza diretorios do dia

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/script.js
@@ -0,0 +1,15 @@
+const objeto = (nomeCompleto) => {
+    const email = nomeCompleto.toLowerCase().split(' ').join('_');
+    return { nomeCompleto, email: `${email}@trybe.com` };
+};
+
+const newEmployees = (callback) => {
+    const employees = {
+        id1: callback('Pedro Guerra'),
+        id2: callback('Luiza Drumond'),
+        id3: callback('Carla Paiva'),
+    };
+    return employees;
+};
+
+console.log(newEmployees(objeto));
