07863312c1a4358a69c92e617056baec671b1de4
Renanbfreitas <renanbf1992@outlook.com>
2022-08-25 23:27:08 -0300

    Organiza diretorios do dia

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -0,0 +1,6 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+console.log(toObject(palio, shelbyCobra, chiron));
