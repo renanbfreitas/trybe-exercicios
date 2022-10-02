cf0d6556184edd290c98de2639a1a76273ced732
Renanbfreitas <renanbf1992@outlook.com>
2022-10-01 21:24:53 -0300

    Organiza diretorios do dia

---
+++ 02-front-end/seçao-1-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise4.js
@@ -0,0 +1,15 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => console.log(`A temperatira de Marte é: ${currentTemperature} graus celcius`), messageDelay());
+};
+
+sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
