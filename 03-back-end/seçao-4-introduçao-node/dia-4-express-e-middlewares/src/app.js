4f7095a6c6b81cc693105401beb7b190df840ab4
Renanbfreitas <renanbf1992@outlook.com>
2023-02-02 18:50:18 -0300

    Organiza diretorios do dia

---
+++ 03-back-end/seçao-4-introduçao-node/dia-4-express-e-middlewares/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const validateDifficulty = require('./middlewares/validateDifficulty');
+const validateRating = require('./middlewares/validateRating');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateDescription = require('./middlewares/validateDescription');
+const validatePrice = require('./middlewares/validatePrice');
+const validateName = require('./middlewares/validateName');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/activities',
+validateDifficulty, 
+validateRating,
+validateCreatedAt,
+validateDescription,
+validateName, 
+validatePrice,
+(_req, res) => {
+res.status(201).json({ message: 'Atividade registrada com sucesso!' });
+});
+
+module.exports = app;
