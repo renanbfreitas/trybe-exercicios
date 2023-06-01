1917835256b12c3b7dc71bc5dd43110cd54bbc1b
Renanbfreitas <renanbf1992@outlook.com>
2023-05-31 23:10:40 -0300

    Organiza diretorios do dia

---
+++ 04-ciencia-da-computacao/seçao-2-algoritmos/dia-3-algoritmos-de-ordenaçao-e-busca/exercise1.py
@@ -0,0 +1,7 @@
+# Dado um array com os seguintes elementos
+# ["zebra", "macaco", "elefante", "arara", "javali"],
+# após duas iterações utilizando bubble sort, como estaria este array?
+
+# Resposta:
+
+# ["elefante", "arara", "javali", "macaco", "zebra"]
